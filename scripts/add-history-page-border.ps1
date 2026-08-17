e64787e357f8c500e5a52aef287b0820dae65615
cunhadalbert-cmyk <cunhadalbert@gmail.com>
2026-08-17 15:12:51 -0300

    Corrige root real na moldura de História

--- scripts/add-history-page-border.ps1
+++ scripts/add-history-page-border.ps1
@@ -4,12 +4,15 @@ param(
 
 $ErrorActionPreference = 'Stop'
 
+$projectRoot = Split-Path -Parent $PSScriptRoot
 $baseScript = Join-Path $PSScriptRoot 'add-geography-page-border.ps1'
 if (-not (Test-Path -LiteralPath $baseScript)) {
     throw ('Script base de moldura nao encontrado: ' + $baseScript)
 }
 
 $source = [IO.File]::ReadAllText($baseScript, [Text.Encoding]::UTF8)
+$escapedRoot = $projectRoot.Replace("'", "''")
+$source = $source.Replace("`$root = Split-Path -Parent `$PSScriptRoot", "`$root = '$escapedRoot'")
 $source = $source.Replace("exports\word\4-ano\geografia", "exports\word\4-ano\historia")
 
 $temp = Join-Path ([IO.Path]::GetTempPath()) ('teacheasy-history-border-' + [guid]::NewGuid().ToString('N') + '.ps1')
